cefac0419f4fc5ae4670c24599d390c61e243925
sumin <sumin1667@gmail.com>
2024-05-04 04:49:33 +0900

    add feat. movie summary length limit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,11 @@ function Movie({id, coverImg, title, summary, genres}){
       <div>
         <img src={coverImg} alt={title}/>
         <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-        <p>{summary}</p>
+        {summary.length > 235 ? (
+          <p>`{summary.slice(0,235)}...`</p> 
+        ) : (
+          <p>{summary}</p>
+        )}
         <ul>
           {genres.map(g => 
           <li key={g}>
